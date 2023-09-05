f8cf6e0133077e67d06f506b9012fb0d79f8e92d
dxkrnn <dxkrnn@gmail.com>
2023-09-06 00:58:20 +0700

    feat: Signin Signup Page

---
+++ app/src/main/java/com/coolyeah/gosave/ui/SigninPage.kt
@@ -0,0 +1,12 @@
+package com.coolyeah.gosave.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.coolyeah.gosave.R
+
+class SigninPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin_page)
+    }
+}
--- app/src/main/java/com/coolyeah/gosave/ui/SignupPage.kt
+++ app/src/main/java/com/coolyeah/gosave/ui/SignupPage.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.coolyeah.gosave.R
 
-class LoginPage : AppCompatActivity() {
+class SignupPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_page)
+        setContentView(R.layout.activity_signup_page)
     }
 }
