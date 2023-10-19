4fcbef215b1ef3ecf8feb4cbff2bd5b18f9bc68d
dxkrnn <dxkrnn@gmail.com>
2023-10-19 20:50:59 +0700

    fix: signin signup

--- app/src/main/java/com/coolyeah/gosave/SigninPage.kt
+++ app/src/main/java/com/coolyeah/gosave/SigninPage.kt
@@ -1,12 +1,20 @@
 package com.coolyeah.gosave
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.coolyeah.gosave.R
 
 class SigninPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin_page)
+
+        val daftarDisini = findViewById<TextView>(R.id.daftar_disini) //
+        daftarDisini.setOnClickListener {
+            val intent = Intent(this, SignupPage::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/coolyeah/gosave/SignupPage.kt
+++ app/src/main/java/com/coolyeah/gosave/SignupPage.kt
@@ -1,12 +1,23 @@
 package com.coolyeah.gosave
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.coolyeah.gosave.R
+import com.coolyeah.gosave.ui.Onboarding_4
 
 class SignupPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_page)
+
+
+        val loginDisini = findViewById<TextView>(R.id.login_disini) //
+        loginDisini.setOnClickListener {
+            val intent = Intent(this, SigninPage::class.java)
+            startActivity(intent)
+        }
     }
 }
